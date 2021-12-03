88a1a29fc8b160731a1f1daeade6359f635e555e
nottolstybai <nottolstybai@gmail.com>
2021-12-03 23:21:53 +0300

    First commit

---
+++ src/main/java/com/example/storehouse/Order.java
@@ -0,0 +1,31 @@
+public class Order {
+    private int id;
+    private String dateTime;
+    private Products product;
+    private int quantity;
+    private boolean direction;
+
+    public Order(int id, String dateTime, Products product, int quantity, boolean direction) {
+        this.id = this.id;
+        this.dateTime = dateTime;
+        this.product = product;
+        this.quantity = quantity;
+        this.direction = direction;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public Products getProduct() {
+        return product;
+    }
+
+    public String getDateTime() {
+        return dateTime;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+}
---
+++ src/main/java/com/example/storehouse/Products.java
@@ -0,0 +1,59 @@
+public class Products {
+    private int id;
+    private String type;
+    private int price;
+    private Person person;
+    private String expireDate;
+    private int quantity;
+    private String location;
+
+    public Products(int id, String type, int price, Person person, String expireDate, int quantity, String location){
+        this.id = id;
+        this.type = type;
+        this.price = price;
+        this.person = person;
+        this.expireDate = expireDate;
+        this.quantity = quantity;
+        this.location = location;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public String getType() {
+        return type;
+    }
+
+    public Person getPerson() {
+        return person;
+    }
+
+    public String getExpireDate() {
+        return expireDate;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+
+    public void setQuantity(int quantity) {
+        this.quantity = quantity;
+    }
+}
